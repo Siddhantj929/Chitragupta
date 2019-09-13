a9fa61c5b9a8757ecebad31d67f296045f641e20
siddhantj929 <siddhantj929@gmail.com>
2019-09-13 14:43:40 +0530

    dockerised the project

--- api/config.py
+++ api/config.py
@@ -1,7 +1,7 @@
 class Config(object):
     DEBUG = False
     TESTING = False
-    DATABASE_URI = 'postgres://root:root@localhost:5432/hoopoun_users'
+    DATABASE_URI = 'postgres://root:root@postgres:5432/chitragupta'
 
 
 class ProductionConfig(Config):
