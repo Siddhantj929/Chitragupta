a5190b02cc7293709a49b3d3f5b26dd1a309ddbc
siddhantj929 <siddhantj929@gmail.com>
2019-09-17 13:47:06 +0530

    created the app

--- api/runserver.py
+++
@@ -1,8 +0,0 @@
-import database
-from app import app
-import api
-
-from config import CURRENT_CONFIG
-
-if __name__ == "__main__":
-    app.run(port=CURRENT_CONFIG.PORT)
--- api/services/user_credentials/model.py
+++
@@ -1,7 +0,0 @@
-from ..base_model import BaseModel
-from .schema import UserCredentialsSchema
-
-
-class UserCredentials(BaseModel):
-    def __init__(self):
-        super().__init__(schema=UserCredentialsSchema)
